c34d6e66c10527adbf49cfc255b9c0c3e3792b5b
Angie014 <ing.Angelica014@gmial.com>
2020-06-07 16:55:37 -0400

    create SearchElement.ps1

---
+++ SearchElement.ps1
@@ -0,0 +1,6 @@
+﻿mkdir C:\POWER_SHELL
+clear
+
+$inicio = Get-Date 06/01/20
+$final = Get-Date 06/01/20
+Get-EventLog -LogName Application -EntryType Information -Newest 100 -Before $final -After $inicio | ConvertTo-Html -Title "Eventos de Windows" -Body (Get-Date) -PreContent "<p>Computadora Angelica</p>" -PostContent "Script hecho por angelica peña"| Out-File 'C:\POWER_SHELL\searchElement.html' 
