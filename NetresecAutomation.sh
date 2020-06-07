8cd451d3a205e028b35bfc3e8f830ec83265d22d
Angie014 <ing.Angelica014@gmial.com>
2020-06-07 17:47:39 -0400

    create Netresec-WiresharkAutomation.sh

---
+++ NetresecAutomation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install libmono-system-windows-forms4.0-cil
+sudo apt-get install libmono-system-web4.0-cil
+sudo apt-get install libmono-system-net4.0-cil
+sudo apt-get install libmono-system-runtime-serialization4.0-cil
+sudo apt-get install libmono-system-xml-linq4.0-cil
